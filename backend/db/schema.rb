6c494e99856d87339c45d156c9d36a74e97debc8
Sky Blue <dawadiaakash@gmail.com>
2025-08-20 22:13:26 +0545

    fix(backend): user reg and login

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_20_145044) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_20_160917) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -20,7 +20,9 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_20_145044) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "jti"
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["jti"], name: "index_users_on_jti"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 end
